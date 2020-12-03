ab9c59813458490d3c78f289179846705ca07b3a
unknown <luisapataquini@hotmail.com>
2020-12-03 11:17:38 -0300

    Navbar, Footer e Home

--- blogPessoal-ui/src/app/app-routing.module.ts
+++ blogPessoal-ui/src/app/app-routing.module.ts
@@ -2,10 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [
-{ path:'', redirectTo:'home', pathMatch:'full' },
-{ path: 'home', component: HomeComponent },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
